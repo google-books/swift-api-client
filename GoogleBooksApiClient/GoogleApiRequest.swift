f6f267c42bbc1de219b904f9acd2f28eeb96f3c2
gomi_ningen <korun24276@gmail.com>
2017-01-09 17:14:20 +0900

    Add GoogleApiRequest#auth,token and GoogleApiToken

---
+++ GoogleBooksApiClient/GoogleApiRequest.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+private let OAUTH2_URL = URL(string: "https://accounts.google.com/o/oauth2")!
+private let BOOK_SCOPE_URL = URL(string: "https://www.googleapis.com/auth/books")!
+
+public struct GoogleApiRequests {
+    
+    private init() {}
+    
+    public static func auth(redirectUrl: URL, clientId: String) -> URLRequest? {
+        var comp = URLComponents(url: OAUTH2_URL.appendingPathComponent("/auth"), resolvingAgainstBaseURL: false)
+        comp?.queryItems = [
+            URLQueryItem(name: "scope", value: BOOK_SCOPE_URL.absoluteString),
+            URLQueryItem(name: "redirect_uri", value: redirectUrl.absoluteString),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "client_id", value: clientId)
+        ]
+        return comp?.url.map({ URLRequest(url: $0) })
+    }
+    
+    public static func token(redirectUrl: URL, clientId: String, clientSecret: String, code: String) -> URLRequest {
+        let params: [String:String] = [
+            "redirect_uri" : redirectUrl.absoluteString,
+            "grant_type" : "authorization_code",
+            "client_id" : clientId,
+            "client_secret" : clientSecret,
+            "code" : code
+        ]
+        let encoded = params
+            .flatMap({ param in
+                guard
+                    let key = param.key.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+                    let value = param.value.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                    else { return nil }
+                return key + "=" + value
+            })
+            .joined(separator: "&")
+        let url = OAUTH2_URL.appendingPathComponent("/token")
+        var req = URLRequest(url: url)
+        req.httpMethod = "POST"
+        req.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        req.httpBody = encoded.data(using: .utf8)
+        return req
+    }
+    
+}
---
+++ GoogleBooksApiClient/GoogleApiToken.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+public struct GoogleApiToken {
+    
+    public let accessToken: String
+    public let expiresIn: Int
+    public let refreshToken: String
+    public let tokenType: String
+    
+}
+
+extension GoogleApiToken: Deserializable {
+    
+    public static func create(_ dict: [AnyHashable : Any]) -> GoogleApiToken? {
+        guard
+            let accessToken = dict["access_token"] as? String,
+            let expiresIn = dict["expires_in"] as? Int,
+            let refreshToken = dict["refresh_token"] as? String,
+            let tokenType = dict["token_type"] as? String
+            else { return nil }
+        return GoogleApiToken(
+            accessToken: accessToken,
+            expiresIn: expiresIn,
+            refreshToken: refreshToken,
+            tokenType: tokenType
+        )
+    }
+    
+}
